01c34be2dcfb7f307546746586cd5ca7cac79e37
Octavio Vera <ofvera@uc.cl>
2025-07-04 21:53:18 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Configuración específica para Vercel
+  experimental: {
+    esmExternals: false
+  }
 }
 
 module.exports = nextConfig
